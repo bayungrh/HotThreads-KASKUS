4f352e4377feb96fe8078bdd2d5403583a6c7cda
MuhBayu <bnugraha00@hotmail.com>
2017-07-15 22:02:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header('Content-Type:Application/Json;UTF-8');
 require_once(__DIR__.'/src/kaskus.php');
 
 $kaskus_ht = new bayyunet\kaskus_api;
-$kaskus_ht->setApiKey('120500'); // Set up your API KEY
+$kaskus_ht->setApiKey('YOUR_API_KEY'); // Set up your API KEY
 $data = $kaskus_ht->Hot_Thread();
 $json = json_decode($data, true); // Parse JSON to ARRAY
 
